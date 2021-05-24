7bd1f49a01ab9a9fcea5394071afd555bfaa45b5
MyUsername <simo.3190@gmail.com>
2021-05-24 16:58:59 +0200

    es disci

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container text-center">
+      <h1>ciaone</h1>
+      <Disco 
+        v-for= "(disco,index) in dischi"
+        :key= "index"
+        :disco= "disco"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Disco from '@/components/Disco.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Disco,
+  },
+  data(){
+    return{
+      axios,
+      dischi:[]
+    }
+  },
+  created (){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(res=>{
+      /* console.log(res.data.response) */
+      this.dischi= res.data
+
+      console.log(this.dischi)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/assets/style/general';
 </style>
---
+++ src/components/Disco.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ disco.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Disco',
+  props:{
+    disco:Object
+  }
+}
+</script>
+
+<style>
+
+</style>
